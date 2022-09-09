7584788399450722e1be28c877a36e9a127db233
olgaolgasson <113113935+olgaolgasson@users.noreply.github.com>
2022-09-09 10:21:55 +0200

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header ('Menue')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+steamlit.text ('Kale, Spinach & ROcket Smoothie')
+streamlit.text ('Hard-Boiled Free-Range Egg')
